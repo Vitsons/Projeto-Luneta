7a853e9ceec72287e431e93e2e8fd466338f56c7
Vitsons <vbmjacome@hotmail.com>
2016-06-04 22:41:50 -0300

    Add files via upload

---
+++ StepperButtonPot.ino
@@ -0,0 +1,85 @@
+/* Código para controle dos motores da luneta
+   com potenciômetro para controle de velocidade
+*/
+
+#include <AccelStepper.h>   // Biblioteca para controle do motor de passo
+
+#define Azul 2
+#define Rosa 3
+#define Amarelo 4
+#define Laranja 5
+#define Button1 6
+#define Button2 7
+#define LED1 8
+#define LED2 9
+#define LED3 10
+#define LED4 11
+#define LED5 12
+
+int but1 = 0, but2 = 0;
+int vel = 0, vel2 = 0;
+int pot = 0;
+
+AccelStepper motor1(8, Azul, Amarelo, Rosa, Laranja);   // Inicialização do primeiro motor
+
+void setup() {
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  motor1.setMaxSpeed(1000.0);   // Velocidade máxima definida
+}
+
+void loop() {
+  
+  // Controle de velocidade:
+   
+  pot = analogRead(A5);
+  vel = pot/1.13; // vel máxima = 905
+  if (vel < 40){
+    vel = 40;     // vel mínima = 40
+  }
+
+  // LEDs:
+  /* Cada LED acende a cada dois números do potenciômetro*/
+
+  digitalWrite(LED1, HIGH);   // LED1 sempre aceso
+  
+  if (vel>=170){
+    digitalWrite(LED2, HIGH);
+  }else{
+    digitalWrite(LED2, LOW);
+  }
+  if (vel>=380){
+    digitalWrite(LED3, HIGH);
+  }else{
+    digitalWrite(LED3, LOW);
+  }
+  if (vel>=600){
+    digitalWrite(LED4, HIGH);
+  }else{
+    digitalWrite(LED4, LOW);
+  }
+  if (vel>=815){
+    digitalWrite(LED5, HIGH);
+  }else{
+    digitalWrite(LED5, LOW);
+  }
+
+  // Botões direcionais:
+  
+  but1 = digitalRead(Button1);
+  but2 = digitalRead(Button2);
+  if (but1 == HIGH){
+    motor1.setSpeed(vel);
+    motor1.runSpeed();
+  }
+  vel2 = -vel;    // Inverter direção
+  if (but2 == HIGH){
+    motor1.setSpeed(vel2);
+    motor1.runSpeed();
+  }
+}
---
+++ StepperButtonSwitch.ino
@@ -0,0 +1,57 @@
+/* Código para controle dos motores da luneta
+   com switch de três posições como controle
+   de velocidade
+*/
+
+#include <AccelStepper.h>
+
+#define Azul 2
+#define Rosa 3
+#define Amarelo 4
+#define Laranja 5
+#define Button1 6
+#define Button2 7
+#define velBaixa 9
+#define velAlta 10
+
+int but1 = 0, but2 = 0;
+int vel = 0, vel2 = 0;
+
+AccelStepper motor1(8, Azul, Amarelo, Rosa, Laranja);   // Inicialização do primeiro motor
+
+void setup() {
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(velBaixa, INPUT);
+  pinMode(velAlta, INPUT);
+  motor1.setMaxSpeed(1000.0);
+}
+
+void loop() {
+
+  // Switch:
+  
+  if (digitalRead(velBaixa) == HIGH) {   // Velocidade Mínima
+    vel = 50.0;
+  } 
+  if (digitalRead(velAlta) == HIGH) {   // Velocidade máxima
+    vel = 900.0;
+  }
+  if (digitalRead(velBaixa) == LOW && digitalRead(velAlta) == LOW) {   // Velocidade média
+    vel = 300.0;
+  }
+
+  // Botões direcionais:
+
+  but1 = digitalRead(Button1);
+  but2 = digitalRead(Button2);
+  if (but1 == HIGH){
+    motor1.setSpeed(vel);
+    motor1.runSpeed();
+  }
+  vel2 = -vel;    // Inverter direção
+  if (but2 == HIGH){
+    motor1.setSpeed(vel2);
+    motor1.runSpeed();
+  }
+}
